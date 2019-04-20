b8b2a7086256cf5ab4ff48017cdabb749fea4c29
Duncan Cock Foster <dcarlcf@gmail.com>
2019-04-20 16:06:41 -0700

    Updated test contract

--- contracts/TestContractCalls.sol
+++ contracts/TestContractCalls.sol
@@ -2,31 +2,31 @@ pragma solidity ^0.5.4;
 
 contract TestContractCalls {
 
-    address ExampleWallet = 0xcc5a36BF104bBE0223367D6bC45e8b936b89357F;
+    address ExampleNiftyWalletAddress = 0x72947f7e561e66E902D9210d6f7261DD06A2Ec96;
 
     function onERC721Received(address add1, address add2, uint256 id, bytes memory dat) public view returns (bytes4) {
-        StaticContract new_c = StaticContract(ExampleWallet);
+        NiftyWallet new_c = NiftyWallet(ExampleNiftyWalletAddress);
         return (new_c.onERC721Received(add1, add2, id, dat));
     }
 
     function add() public view returns(address) {
-        StaticContract new_c = StaticContract(ExampleWallet);
+        NiftyWallet new_c = NiftyWallet(ExampleNiftyWalletAddress);
         return (new_c.retAdd());
     }
 
     function isValidSignature(bytes calldata hash, bytes calldata signature) external view returns (bytes4) {
-        StaticContract new_c = StaticContract(ExampleWallet);
+        NiftyWallet new_c = NiftyWallet(ExampleNiftyWalletAddress);
         return (new_c.isValidSignature(hash, signature));
     }
 
     function isValidSignature(bytes32 hash, bytes calldata signature) external view returns (bool) {
-        StaticContract new_c = StaticContract(ExampleWallet);
+        NiftyWallet new_c = NiftyWallet(ExampleNiftyWalletAddress);
         return (new_c.isValidSignature(hash, signature));
     }
 
 }
 
-contract StaticContract {
+contract NiftyWallet {
     function onERC721Received(address, address, uint256, bytes memory) public view returns (bytes4);
     function isValidSignature(bytes32 hash, bytes calldata signature) external view returns (bool);
     function isValidSignature(bytes calldata hash, bytes calldata signature) external view returns (bytes4);
